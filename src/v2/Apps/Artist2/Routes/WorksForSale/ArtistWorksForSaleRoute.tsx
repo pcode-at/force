6f7a07369009c413f213eb90240f7bd0e7ec5138
Christopher Pappas <damassi.pappas@gmail.com>
2021-05-26 12:45:27 -0700

    wip

--- src/v2/Apps/Artist2/Routes/WorksForSale/ArtistWorksForSaleRoute.tsx
+++ src/v2/Apps/Artist2/Routes/WorksForSale/ArtistWorksForSaleRoute.tsx
@@ -20,6 +20,25 @@ export const ArtistWorksForSaleRouteFragmentContainer = createFragmentContainer(
           page: { type: "Int" }
           aggregations: { type: "[ArtworkAggregation]" }
         ) {
+        ...Artist2ArtworkFilter_artist @arguments(input: $input)
+
+        sidebarAggregations: filterArtworksConnection(
+          sort: $sort
+          page: $page
+          aggregations: $aggregations
+          first: 1
+          after: ""
+        ) {
+          aggregations {
+            slice
+            counts {
+              name
+              value
+              count
+            }
+          }
+        }
+
         internalID
         slug
         id
